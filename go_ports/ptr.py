117de337da250db6fb9a0c26e4fcd4126731c172
Josh Holbrook <josh.holbrook@gmail.com>
2024-11-15 19:37:13 -0900

    Improve Ptr docs

--- go_ports/ptr.py
+++ go_ports/ptr.py
@@ -1,7 +1,17 @@
 class Ptr[V]:
     """
-    A pointer. Go has pointers, Python does not. In cases where a value needs
-    to be updated by reference, this class can help.
+    A pointer. Go has pointers, Python does not. This class assists in
+    use cases involving pointers, such as in passing a reference to a
+    function which mutates its value:
+
+    ```py
+    p = Ptr(False)
+
+    set_to_true(p)
+
+    # This will succeed
+    assert p.deref()
+    ```
 
     Note that this is NOT a true pointer - it won't update the value of a
     wrapped variable. For example:
@@ -15,7 +25,7 @@ class Ptr[V]:
     assert value
     ```
 
-    Instead, create the value as a pointer, and use the pointer to set
+    Instead, you must create the value as a pointer, and use the pointer to set
     the value:
 
     ```
